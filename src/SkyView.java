a16c310cb8ed17267f49ce8260e69622a643ebf9
Harpreet Kaur <hkaur0745@bths.edu>
2019-01-21 12:32:46 -0500

    Initial commit

---
+++ src/SkyView.java
@@ -0,0 +1,49 @@
+public class SkyView {private double [][] view;
+    public SkyView(int numRows, int numCols, double[] scanned)
+    {
+        view = new double[numRows][numCols];
+        int scannedIndex = 0;
+        for(int row = 0; row < numRows; row++)
+        {
+            if( row % 2 == 0)
+            {
+                for(int col = 0; col < numCols; col++)
+                {
+                    view[row][col] = scanned[scannedIndex];
+                    scannedIndex++;
+                }
+            }
+            else
+            {
+                for(int col = numCols - 1; col >= 0; col--)
+                {
+                    view[row][col] = scanned[scannedIndex];
+                    scannedIndex++;
+                }
+            }
+            for (int col = 0; col < view[0].length; col++)
+            {
+                System.out.print(view[row][col] + " ");
+            }
+            System.out.println();
+        }
+    }
+    public double getAverage(int startRow, int endRow, int startCol, int endCol)
+    {
+        double sum = 0;
+        int count = 0;
+
+        for(int row = startRow; row <= endRow; row++)
+        {
+            for(int col = startCol; col <= endCol; col++)
+            {
+                sum += view[row][col];
+                count++;
+            }
+        }
+
+        return sum / count;
+    }
+}
+
+
