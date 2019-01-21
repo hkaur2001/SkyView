fd6f36236fe95d1ef1fa6dfc092549f11273bb50
Harpreet Kaur <hkaur0745@bths.edu>
2019-01-21 12:35:11 -0500

    good

--- src/Runner.java
+++ src/Runner.java
@@ -1,7 +1,7 @@
 public class Runner {
         public static void main(String[] args){
-            double[] scan = {0.3,0.7,0.8,0.4,1.4,1.1, 0.1,0.2, 0.2, 1.6, 0.5, 0.9};
-            double[] scan2 = {0.3, 0.5, 0.7, 0.4, 1.4, 1.1};
+            double[] scan = {0.3,0.7,0.834,0.4,1.4,1.1, 0.1,0.2, 0.2, 1.6, 0.5, 0.9};
+            double[] scan2 = {0.3, 0.5, 0.75, 0.4, 1.44, 1.1};
 
             SkyView sv1 = new SkyView(4,3,scan);
             System.out.println();
